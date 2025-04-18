bcb890cbb264b3df59a4f0070821dac753730ea7
Rishabh Roy <rishabh25126@gmail.com>
2025-04-18 10:24:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js!');
+});
+
+app.listen(PORT,'0.0.0.0', () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
